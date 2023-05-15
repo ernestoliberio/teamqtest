3a0b2b562782c856b3735149e8b15ea36635b0ce
Ernesto Liberio <elv@team.biz>
2023-05-14 22:26:19 -0500

    initial

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return AnonymousResourceCollection
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        $posts = Post::all();
+        return PostResource::collection($posts);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return PostResource
+     */
+    public function store(StorePostRequest $request): PostResource
+    {
+        $posts = Post::create($request->all());
+
+        return new PostResource($posts);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return PostResource
+     */
+    public function show(Post $post): PostResource
+    {
+        return new PostResource($post);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param  \App\Models\Post  $post
+     * @return PostResource
+     */
+    public function update(StorePostRequest $request, Post $post): PostResource
+    {
+        $post->update($request->all());
+
+        return new PostResource($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post): \Illuminate\Http\Response
+    {
+        $post->delete();
+
+        return response(null, 204);
+    }
+}
