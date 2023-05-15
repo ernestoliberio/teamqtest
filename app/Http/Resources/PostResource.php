453d14a7ca5bb6b262f15a2af27c0ea3aedb9875
Ernesto Liberio <elv@team.biz>
2023-05-14 22:59:27 -0500

    test

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -7,13 +7,5 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PostResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return parent::toArray($request);
-    }
+
 }
